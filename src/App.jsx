91231a0f0440fdcffb098daa52082db0ecaea285
Sofiane DEBRABANT <sofiane.debrabant.pro@gmail.com>
2025-01-28 17:14:47 +0100

    Initialisation du frontend avec Vite et Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold text-blue-500">Flashcard App</h1>
+    </div>
+  );
+}
+
+export default App;
